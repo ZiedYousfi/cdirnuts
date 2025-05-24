a2683baefea37e5d603e9aa6146b80ac155471b7
ZiedYousfi <yousfi_zied@yahoo.com>
2025-05-24 19:10:15 +0200

    Refactor main function to improve current directory retrieval and enhance code readability

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-
   (void)argc;
   (void)argv;
 
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("Dossier courant lors de l'appel : %s\n", cwd);
+  } else {
+    perror("getcwd() error");
+    return 1;
+  }
+
   printf("Hello, World!\n");
-  
+
   // Return 0 to indicate successful execution
   return 0;
 }
