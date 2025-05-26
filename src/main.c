fc77f78bc19687520c23cd95f58bc46b7342a7ea
ZiedYousfi <yousfi_zied@yahoo.com>
2025-05-26 12:05:17 +0200

    Refactor main function to improve argument handling and logging; enhance readability and structure

--- src/main.c
+++ src/main.c
@@ -1,24 +1,58 @@
 #include <limits.h>
 #include <unistd.h>
 
-#include "../include/log.h"
 #include "../include/init.h"
+#include "../include/log.h"
+
+#define PROJECT_NAME "my_project"
 
 int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
+  char *projectName = PROJECT_NAME;
 
-  char cwd[PATH_MAX];
-  if (getcwd(cwd, sizeof(cwd)) != NULL) {
-    log_message(LOG_INFO, "Current directory at startup: %s", cwd);
+  if (argc > 1) {
+    if (argv[1][0] == '-') {
+      if (strcmp(argv[1], "--help") == 0) {
+        /* code for help */
+      } else if (strcmp(argv[1], "--config") == 0) {
+        if (argc <= 2) {
+          log_message(
+              LOG_ERROR,
+              "--config as first argument but no config file path provided");
+          return -1;
+        }
+        log_message(LOG_INFO, "Selected config : %s", argv[2]);
+      }
+    } else {
+      projectName = argv[1];
+      char cwd[PATH_MAX];
+      if (strchr(projectName, '/')) {
+        // TODO: handle path in arg
+      } else {
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+          log_message(LOG_INFO, "Current directory at startup: %s", cwd);
+        } else {
+          log_message(LOG_ERROR, "getcwd() error");
+          return 1;
+        }
+      }
+      if (init_default_setup(cwd, projectName) != 0) {
+        log_message(LOG_ERROR, "Failed to initialize default setup.");
+        return 1;
+      }
+    }
   } else {
-    log_message(LOG_ERROR, "getcwd() error");
-    return 1;
-  }
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      log_message(LOG_INFO, "Current directory at startup: %s", cwd);
+    } else {
+      log_message(LOG_ERROR, "getcwd() error");
+      return 1;
+    }
 
-  if (init_default_setup(cwd, "my_project") != 0) {
-    log_message(LOG_ERROR, "Failed to initialize default setup.");
-    return 1;
+    if (init_default_setup(cwd, "my_project") != 0) {
+      log_message(LOG_ERROR, "Failed to initialize default setup.");
+      return 1;
+    }
   }
 
   log_message(LOG_INFO, "Hello, World!");
