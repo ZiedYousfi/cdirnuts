d6f8e89a90e6445b092f65d688c723041c6e8617
ZiedYousfi <yousfi_zied@yahoo.com>
2025-05-26 13:36:23 +0200

    Refactor main function to remove commented code and improve clarity; enhance error handling for config argument

--- src/main.c
+++ src/main.c
@@ -9,82 +9,77 @@
 #define PROJECT_NAME "my_project"
 
 int main(int argc, char *argv[]) {
-  (void)argc; // Suppress unused parameter warning
-  (void)argv; // Suppress unused parameter warning
-// Actual main function code is commented out for now.
-// Uncomment the following lines to use the main function code.
-//   int result = 0;
-//   char *projectName = PROJECT_NAME;
+  int result = 0;
+  char *projectName = PROJECT_NAME;
 
-//   if (argc > 1) {
-//     if (argv[1][0] == '-') {
-//       if (strcmp(argv[1], "--help") == 0) {
-//         /* code for help */
-//       } else if (strcmp(argv[1], "--config") == 0) {
-//         if (argc <= 2) {
-//           log_message(
-//               LOG_ERROR,
-//               "--config as first argument but no config file path
-//               provided");
-//           result = 1;
-//           goto cleanup_main;
-//         }
-//         log_message(LOG_INFO, "Selected config : %s", argv[2]);
-//       }
-//     } else {
-//       projectName = argv[1];
-//       char cwd[PATH_MAX];
-//       if (strchr(projectName, '/')) {
-//         // TODO: Handle case where projectName contains a path
-//       } else {
-//         if (getcwd(cwd, sizeof(cwd)) != NULL) {
-//           log_message(LOG_INFO, "Current directory at startup: %s", cwd);
-//         } else {
-//           log_message(LOG_ERROR, "getcwd() error");
-//           result = 1;
-//           goto cleanup_main;
-//         }
-//       }
-//       if (init_default_setup(cwd, projectName) != 0) {
-//         log_message(LOG_ERROR, "Failed to initialize default setup.");
-//         result = 1;
-//         goto cleanup_main;
-//       }
-//     }
-//   } else {
-//     char cwd[PATH_MAX];
-//     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-//       log_message(LOG_INFO, "Current directory at startup: %s", cwd);
-//     } else {
-//       log_message(LOG_ERROR, "getcwd() error");
-//       result = 1;
-//       goto cleanup_main;
-//     }
-
-//     if (init_default_setup(cwd, "my_project") != 0) {
-//       log_message(LOG_ERROR, "Failed to initialize default setup.");
-//       result = 1;
-//       goto cleanup_main;
-//     }
-//   }
-
-//   log_message(LOG_INFO, "Hello, World!");
-
-// cleanup_main:
-//   if (projectName) free(projectName);
-//   return result;
-
-  char pathTest[] = "/home/user/projects/my_project/src/main.c";
-  PathInfo *pathInfo = parsePath(pathTest);
-  if (pathInfo) {
-    printf("Parent Path: %s\n", pathInfo->parentPath);
-    printf("Name: %s\n", pathInfo->name);
-    printf("Is Directory: %s\n", pathInfo->isDirectory ? "true" : "false");
-    freePathInfo(pathInfo);
+  if (argc > 1) {
+    if (argv[1][0] == '-') {
+      if (strcmp(argv[1], "--help") == 0) {
+        /* code for help */
+      } else if (strcmp(argv[1], "--config") == 0) {
+        if (argc <= 2) {
+          log_message(
+              LOG_ERROR,
+              "--config as first argument but no config file path provided");
+          result = 1;
+          goto cleanup_main;
+        }
+        log_message(LOG_INFO, "Selected config : %s", argv[2]);
+      }
+    } else {
+      projectName = argv[1];
+      char cwd[PATH_MAX];
+      if (strchr(projectName, '/')) {
+        // TODO: Handle case where projectName contains a path
+      } else {
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+          log_message(LOG_INFO, "Current directory at startup: %s", cwd);
+        } else {
+          log_message(LOG_ERROR, "getcwd() error");
+          result = 1;
+          goto cleanup_main;
+        }
+      }
+      if (init_default_setup(cwd, projectName) != 0) {
+        log_message(LOG_ERROR, "Failed to initialize default setup.");
+        result = 1;
+        goto cleanup_main;
+      }
+    }
   } else {
-    printf("Failed to parse path.\n");
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+      log_message(LOG_INFO, "Current directory at startup: %s", cwd);
+    } else {
+      log_message(LOG_ERROR, "getcwd() error");
+      result = 1;
+      goto cleanup_main;
+    }
+
+    if (init_default_setup(cwd, "my_project") != 0) {
+      log_message(LOG_ERROR, "Failed to initialize default setup.");
+      result = 1;
+      goto cleanup_main;
+    }
   }
-  return 0;
+
+  log_message(LOG_INFO, "Hello, World!");
+
+cleanup_main:
+  if (projectName) free(projectName);
+  return result;
+
+  // char pathTest[] = "/home/user/projects/my_project/src/main/";
+  // PathInfo *pathInfo = parsePath(pathTest);
+  // if (pathInfo) {
+  //   printf("Parent Path: %s\n", pathInfo->parentPath);
+  //   printf("Name: %s\n", pathInfo->name);
+  //   printf("Is Directory: %s\n", pathInfo->isDirectory ? "true" : "false");
+  //   freePathInfo(pathInfo);
+  // } else {
+  //   printf("Failed to parse path.\n");
+  // }
+  // return 0;
   // Uncomment the following lines to test the directory and file creation
   // if (createDir("test_dir", ".")) {
   //   printf("Directory created successfully.\n");
