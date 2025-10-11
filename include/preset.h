bc8fd1c24754af315c0448d36e8a083fd24c354c
ZiedYousfi <yousfi_zied@yahoo.com>
2025-10-11 18:07:30 +0200

    Add preset structure and function declarations for preset management

---
+++ include/preset.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * Structure representing a preset configuration with a name and associated
+ * path. Will be formated as "name","path" and separated by a new line in .cdndb
+ * file.
+ */
+typedef struct Preset {
+  char *name;
+  char *path;
+} Preset;
+
+Preset *getPresets();
+int savePresets(Preset *presets, int count);
+Preset fromString(const char *str);
+char *toString(Preset preset);
---
+++ src/preset.c
@@ -0,0 +1,18 @@
+#include "../include/preset.h"
+
+Preset *getPresets() {
+  // TODO: Implement this function
+}
+
+int savePresets(Preset *presets, int count) {
+  // TODO: Implement this function
+}
+
+Preset fromString(const char *str) {
+  Preset preset = {NULL, NULL};
+  // TODO: Implement this function
+}
+
+char *toString(Preset preset) {
+  // TODO: Implement this function
+}
